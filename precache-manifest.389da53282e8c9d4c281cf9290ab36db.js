75f1ac82d57f1723f8bd2c449dd691db1f24e4af
senbitronix <60894725+senbitronix@users.noreply.github.com>
2021-11-11 14:09:30 +0300

    Add files via upload

---
+++ precache-manifest.389da53282e8c9d4c281cf9290ab36db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae95e323099e93a1890f40baff68a43e",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "8732d7eacc2fbe8f9b33",
+    "url": "/todo-react/static/css/main.299c5a0c.chunk.css"
+  },
+  {
+    "revision": "ecf9c7ba341e5b4bd5ed",
+    "url": "/todo-react/static/js/2.a3706892.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.a3706892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8732d7eacc2fbe8f9b33",
+    "url": "/todo-react/static/js/main.521a1a72.chunk.js"
+  },
+  {
+    "revision": "39d51d9bc1349eb056a9",
+    "url": "/todo-react/static/js/runtime-main.b10d5a97.js"
+  }
+]);
